cbb0e635d3106aae3e547cdee02603760279c957
OuKai <t110ac1013@ntut.org.tw>
2022-04-01 10:06:05 +0800

    HW3

--- IfElseTest.cs
+++ IfElseTest.cs
@@ -5,3 +5,13 @@ if (a + b > 10)
 {
     Console.WriteLine("The answer is greater than 10");
 }
+
+else if (a - b < 1)
+{
+    Console.WriteLine("The answer is lower than 1");
+}
+
+else 
+{
+    Console.WriteLine("Please change the number a and b");
+}
